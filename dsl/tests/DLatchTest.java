eea499f898b2d2483dedf8272d2e09d58dcf67d1
Alex Reinking <alexander.reinking@yale.edu>
2014-04-27 14:20:08 -0400

    Added integration test

--- dsl/tests/DLatchTest.java
+++ dsl/tests/DLatchTest.java
@@ -4,14 +4,17 @@ import boolex.logic.elements.circuitbuilder.BLXCircuit;
 import boolex.logic.elements.circuitbuilder.BLXModelGenerator;
 import boolex.logic.elements.core.BLXEventManager;
 import boolex.logic.elements.core.BLXSocket;
+import boolex.logic.elements.signals.BLXSignalReceiver;
 import boolex.typechecker.BooLeXTypeChecker;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
+import java.lang.management.ManagementFactory;
+import java.util.*;
+
+import static org.junit.Assert.fail;
 
 public class DLatchTest {
     private DLatchRunner dlatch;
@@ -36,14 +39,13 @@ public class DLatchTest {
         dlatch = new DLatchRunner(inputs.get(0), inputs.get(1));
     }
 
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
     @Test
     public void testDLatch() throws Exception {
-        dlatch.start();
+        try {
+            dlatch.start();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 
     private class DLatchRunner {
@@ -60,14 +62,72 @@ public class DLatchTest {
             this.clockValue = false;
         }
 
-        public void start() {
-            this.eventManager = new BLXEventManager(10);
+        private <K,V> boolean stable(Map<K, V> m1, Map<K, V> m2) {
+            if (m1.size() != m2.size())
+                return false;
+            for (K key: m1.keySet())
+                if (!m1.get(key).equals(m2.get(key)))
+                    return false;
+            return true;
+        }
+
+        private Map<String, Boolean> getSocketMap(Set<BLXSignalReceiver> components) {
+            Map<String, Boolean> socketMap = new HashMap<>();
+            if (components != null) {
+                components.stream().filter(component -> component instanceof BLXSocket).forEach((BLXSignalReceiver component) -> {
+                    BLXSocket socket = (BLXSocket) component;
+                    if (socket.getId() != null && !socket.getId().equals("_"))
+                        socketMap.put(socket.getId(), socket.getValue());
+                });
+            }
+            return socketMap;
+        }
+
+        private Map<String, Boolean> currentStates = new TreeMap<>();
+        private Map<String, Boolean> lastStates = null;
+
+        private void printState(Map<String, Boolean> currentValues) {
+            String output = "";
+
+            for (Map.Entry<String, Boolean> socket : currentValues.entrySet())
+                if (!socket.getKey().equals(""))
+                    output += socket.getKey() + ": " + socket.getValue() + ", ";
+
+            if (!output.equals(""))
+                System.out.println("[" + output + "]");
+        }
+
+        private String error = "";
+        private int stableRun = 0;
+
+        public void start() throws Exception {
+            this.eventManager = new BLXEventManager(10, components -> {
+                currentStates.putAll(getSocketMap(components));
+                if(lastStates == null) {
+                    lastStates = new TreeMap<>(currentStates);
+                    return;
+                }
+
+                boolean isStable = stable(lastStates, currentStates);
+                if(isStable)
+                    stableRun++;
+                if(isStable && stableRun == 10) {
+                    // testing invariants
+                    if(currentStates.get("d") && currentStates.get("clk"))
+                        if (!currentStates.get("%o1") || currentStates.get("%o2"))
+                            error = "d = true and clk = true should => o1 = true and o2 = false.";
+
+                    System.out.print("Stabilized! ");
+                    printState(currentStates);
+                    stableRun = 0;
+                }
+
+                lastStates = new TreeMap<>(currentStates);
+            });
 
             eventManager.start();
             eventManager.update(dataSocket, dataValue);
             eventManager.update(clockSocket, clockValue);
-
-            System.out.println("Simulating: data = " + dataValue + ", clock = " + clockValue);
 
             for (int i = 0; i < 10; i++) {
                 try {
@@ -83,8 +143,15 @@ public class DLatchTest {
                     clockValue = false;
                 }
 
+                if(!error.equals("")) {
+                    eventManager.stop();
+                    throw new AssertionError(error);
+                }
+
                 eventManager.update(dataSocket, dataValue);
                 eventManager.update(clockSocket, clockValue);
+
+                System.out.println(ManagementFactory.getThreadMXBean().getThreadCount());
             }
             eventManager.stop();
         }
